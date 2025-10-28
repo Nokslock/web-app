76cc202bb5c0bac94db9c1ee8cd7984b231535b7
unkwnblu <dagbale16@gmail.com>
2025-10-28 09:48:27 +0100

    file-restructure

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,14 @@
-import { createClient } from "@/lib/supabase/server";
-import SignOutButton from "./auth/sign-out-button"; // Adjust path
 import Link from "next/link";
 
 export default async function HomePage() {
-  const supabase = createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+
 
   return (
-    <div>
-      {user ? (
-        <div>
-          <h1>Welcome back, {user.email}!</h1>
-          <SignOutButton />
-          <br />
-          <Link href="/protected">Go to Protected Page</Link>
-        </div>
-      ) : (
-        <div>
-          <h1>You are not logged in.</h1>
-          <Link href="/login">
+    <>
+      <div>Home Page</div>
+                <Link href="auth/login">
             <button>Login</button>
           </Link>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
