c23b429bb35f8f24be5afc7e928498c91291af5f
unkwnblu <dagbale16@gmail.com>
2025-10-29 02:26:19 +0100

    styling-fix

--- app/auth/login/loginForm.tsx
+++ app/auth/login/loginForm.tsx
@@ -3,18 +3,32 @@ export default function LoginForm() {
   return (
     <>
       <form action="">
-        <div>
-          <label>Email</label>
-          <input type="email" placeholder="Your Email" />
+        <div className="pb-5">
+          <label className="block text-sm font-bold text-gray-500">Email</label>
+          <input
+            type="email"
+            placeholder="Your Email"
+            className="mt-1 w-full px-4 p-2  h-13 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
+          />
+        </div>
+
+        <div  className="pb-5">
+          <label className="block text-sm font-bold text-gray-500">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Your Password"
+            className="mt-1 w-full px-4 p-2  h-13 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
+          />
+        </div>
+
+        <div className="pb-5">
+            <p className="text-md text-end text-blue-400">Forgot Password?</p>
         </div>
 
         <div>
-            <label>Password</label>
-            <input type="password" placeholder="Your Password"/>
-        </div>
-
-        <div>
-            <AuthButton type="login" loading={false} />
+          <AuthButton type="login" loading={false} />
         </div>
       </form>
     </>
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,44 +3,46 @@ import LoginApple from "./loginApple";
 import LoginForm from "./loginForm";
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
-import BgImg from "@/public/login-bg-img.jpg";
-
+import BgImg from "@/public/login-bg-img.png";
 
 export default function LoginPage() {
   return (
-    <div className="container">
-     
-
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-          <Image 
-      src={Logo}
-      alt="A description of my hero image"
-      width={150}
-      height={60}
-     />
-          <div>
-            <h2>Welcome Back</h2>
-            <p>
-              Log in to a simpler, more secure online experience. Your digital
-              world, protected and organized.
-            </p>
+    <>
+      <div className="container mx-auto p-20">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="col-span-1">
+            <div className="pb-30">
+              <Image
+              src={Logo}
+              alt="A description of my hero image"
+              width={150}
+              height={60}
+            />
+            </div>
+            
+            <div className="text-center px-20">
+              <p className="lg:text-5xl md:text-4xl font-bold">Welcome Back</p>
+              
+                <p className="text-lg text-center break-all font-thin py-5 ">
+                  Log in to a simpler, more secure online experience.<br /> Your
+                  digital world, protected and organized.
+                </p>
+                
+            </div>
+            <div className="px-25">
+              <LoginForm />
+            </div>
+            
           </div>
-          <LoginForm />
-        </div>
-        <div>
-          <Image 
-      src={BgImg}
-      alt="A description of my hero image"
-      width={600}
-      height={400}
-     />
-          <div>
-            <LoginGoogle />
-            <LoginApple />
+          <div className="col-span-1">
+            <Image
+              src={BgImg}
+              alt="A description of my hero image"
+              className="max-h-full"
+            />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
